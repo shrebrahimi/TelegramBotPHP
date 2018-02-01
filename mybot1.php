48f0658a5d789c556d95f0131453fd15b1da4631
shrebrahimi <35490418+shrebrahimi@users.noreply.github.com>
2018-02-01 14:42:07 +0330

    Update mybot1.php

--- mybot1.php
+++ mybot1.php
@@ -1,23 +1,11 @@
 <?php
 
 include("Telegram.php");
-data_default_timezone_set("asia/tehran");
 
 $bot_id = "511786967:AAFER0h8tLym9p-F2SsNc2-v2bt5kbWEt-E";
 
 $telegram = new Telegram($bot_id);
 
-$result = $telegram->getData();
-
-$text                       = $telegram->Text();
-$chat_id                    = $telegram->ChatID();
-$username                   = $telegram->Username();
-$name                       = $telegram->FirstName();
-$family                     = $telegram->LastName();
-$callback_data              = $telegram->Callback_Data();
-$callback_query             = $telegram->Callback_Query();
-$callback_chat_id           = $telegram->Callback_ChatID();
-$callback_id                = $telegram->Callback_ID();
-$message_id                 = $telegram->MessageID();
-$user_id                    = $telegram->UserID();
-
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);
